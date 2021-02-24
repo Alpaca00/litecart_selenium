9c9b362b20efd9f872c2387cb4a8de6253fd5d20
Alpaca00 <alpaca00tuha@gmail.com>
2021-02-24 09:18:51 +0200

    homework up to 5 part 1 finished him

--- tests/ui/test_users_registration.py
+++ tests/ui/test_users_registration.py
@@ -7,9 +7,4 @@ class TestUserRegistration:
     def test_user_registration(self):
         first_person = UserRegistration(self.driver)
         first_person.go()
-        assert first_person.to_register()
-
-    def test_user_login_to_account(self):
-        first_person = UserRegistration(self.driver)
-        first_person.go()
-        assert first_person.login_to_account_and_exit()
+        assert first_person.to_register_and_logout_after_re_login_and_logout()
